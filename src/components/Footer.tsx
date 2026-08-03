2c9b469d2b5a102526214610a8852114ef42f3f3
Your Name <you@example.com>
2026-08-03 09:42:08 +0100

    Use 062 42 77 78 as the footer phone number instead of 077 17 69 29

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -125,8 +125,8 @@ export function Footer() {
               </li>
               <li className="flex gap-3">
                 <Phone className="mt-0.5 size-4 shrink-0 text-brand-green" />
-                <a href="tel:+24177176929" className="hover:text-brand-yellow">
-                  +241 77 17 69 29
+                <a href="tel:+24162427778" className="hover:text-brand-yellow">
+                  +241 62 42 77 78
                 </a>
               </li>
               <li className="flex gap-3">
